51c226cd920d315b0e0a84b205e834c75edb1716
Som S. <somwrks@gmail.com>
2024-06-05 11:11:04 +0000

    workin

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { ClerkProvider, SignInButton, SignedIn, SignedOut } from '@clerk/nextjs'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -14,8 +15,36 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+   <ClerkProvider publishableKey='pk_test_dW5pcXVlLWdvYmxpbi04Mi5jbGVyay5hY2NvdW50cy5kZXYk'>
+      <SignedOut>
+        <html>
+          <body>
+            <div className="flex text-white bg-[#001F30] flex-col w-full gap-y-5 min-h-screen items-center p-5 justify-center">
+              <div className="flex flex-col gap-y-4">
+              <div className="flex text-center text-5xl flex-col">
+                RL-Maze  
+              </div>
+              <div className="flex text-center text-2xl flex-col">
+                 Car Maze Simulation
+              </div>
+
+              </div>
+              <div className='p-3 border text-md rounded-2xl'>
+            <SignInButton>
+          
+              Sign in
+              </SignInButton>
+
+              </div>
+            </div>
+          </body>
+        </html>
+      </SignedOut>
+      <SignedIn>
+      <html lang="en">
+      <body style={{background:"#001F30"}} className={inter.className}>{children}</body>
     </html>
+    </SignedIn>
+     </ClerkProvider>
   )
 }

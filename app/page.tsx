7843fb9a968513e8f74ff49c425fa1797452b699
Som S. <somwrks@gmail.com>
2024-06-05 11:22:22 +0000

    workin

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 
 export default function Home() {
   return (
-    <main className="flex  min-h-screen  flex-row items-center justify-between w-full p-24">
-     <div className="flex flex-col w-1/2 border-r-2 p-2">
+    <main className="flex space-y-4 min-h-screen  flex-col items-center  w-full p-24">
+      <h1 className="text-6xl text-center text-white">Select Maze</h1>
+      <div className="flex flex-row justify-between w-full ">
 
-     </div>
-     <div className="flex flex-col w-full">
+     <div className="flex flex-col w-1/2 border-r-2 p-2">
       
      </div>
+     <div className="flex flex-col w-full">
+
+     </div>
+      </div>
     </main>
   )
 }
